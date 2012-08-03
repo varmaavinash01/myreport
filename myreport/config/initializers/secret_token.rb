7d3b8cdc4b5bad9a77f43cada243fc4f0b4be56f
avinash.varma <avinash.varma4464@gmail.com>
2012-08-03 16:23:56 +0000

    initial commit

---
+++ myreport/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myreport::Application.config.secret_token = '017d76343e8014e52db05a9fbfcb2bdce40bee5d45361b398ebce59abdfad189be26b1fa3995db27637e6489a728cffd97eef368e84919929beb517e3b271c73'
